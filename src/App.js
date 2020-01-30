32616b17f47701549e140a0f5a8680bdedc033df
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-01-29 23:51:05 -0600

    Contexts written out.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Contexts
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [ products ] = useState(data);
@@ -21,14 +22,16 @@ function App() {
 
 	return (
 		<div className='App'>
-			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart, removeItem }}>
+				<ProductContext.Provider value={{ products, addItem }}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path='/' component={Products} />
+					{/* Routes */}
+					<Route exact path='/' component={Products} />
 
-				<Route path='/cart' component={ShoppingCart} />
-			</ProductContext.Provider>
+					<Route path='/cart' component={ShoppingCart} />
+				</ProductContext.Provider>
+			</CartContext.Provider>
 		</div>
 	);
 }
